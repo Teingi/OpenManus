bc6d715c7c94269d8f71dc27b6f6082fd4176271
渠磊 <xuyan.wxy@oceanbase.com>
2025-04-07 10:44:49 +0800

    obrag

--- app/obrag/doc_rag.py
+++ app/obrag/doc_rag.py
@@ -338,3 +338,57 @@ def doc_rag_stream(
 
     for suffix in suffixes:
         yield AIMessageChunk(content=suffix + "\n")
+
+
+def query_qe(query):
+    History = []
+    oceanbase_only = True
+    rerank = False
+    search_docs = True
+    lang = "zh"
+    show_refs = True
+    llm_model = "deepseek-v3"
+    res=doc_rag_stream(
+        query=query,
+        chat_history=History,
+        universal_rag=not oceanbase_only,
+        rerank=rerank,
+        llm_model=llm_model,
+        search_docs=search_docs,
+        lang=lang,
+        show_refs=show_refs,
+    )
+    for chunk in res:
+        if isinstance(chunk, AIMessageChunk):
+            yield chunk.content,None
+        else:
+            yield None,chunk
+
+
+if __name__ == "__main__":
+    info=query_qe("obd的使用方法")
+    chunk_all = ""
+    content_all = ""
+    for i in info:
+        content = i[0]
+        chunk = i[1]
+        if content is not None:
+            content_all += content
+        if chunk is not None:
+            chunk_all += chunk
+    print("chunk_all",chunk_all)
+
+    print("content_all",content_all)
+    info=query_qe("你好")
+    chunk_all = ""
+    content_all = ""
+    for i in info:
+        content = i[0]
+        chunk = i[1]
+        if content is not None:
+            content_all += content
+        if chunk is not None:
+            chunk_all += chunk
+    print("chunk_all",chunk_all)
+
+    print("content_all",content_all)
